f09be2b595cd8ccfaee05b838f5869225737bbfe
Marko Milic <marko.milic.88@gmail.com>
2024-05-20 13:27:46 +0200

    Bug fix in calculating player stats

--- LA_LIGA_REKREATIVO/Server/Services/PlayersStatsService.cs
+++ LA_LIGA_REKREATIVO/Server/Services/PlayersStatsService.cs
@@ -61,7 +61,7 @@ namespace LA_LIGA_REKREATIVO.Server.Services
 
         public PlayerStatsDto GetPlayerStats(int id)
         {
-            var player = _context.Players.Include(x => x.Team).Include(x => x.Matches).ThenInclude(x => x.Summaries).FirstOrDefault(x => x.Id == id);
+            var player = _context.Players.Include(x => x.Team).Include(x => x.Matches).ThenInclude(x => x.Summaries).ThenInclude(x=> x.Player).FirstOrDefault(x => x.Id == id);
             var playerStats = player.Matches.Select(x => x.Summaries);
             PlayerStatsDto returnPlayer = new PlayerStatsDto();
             returnPlayer.TotalMatches = player.Matches.Count();
@@ -78,38 +78,41 @@ namespace LA_LIGA_REKREATIVO.Server.Services
             {
                 foreach (var stat in stats)
                 {
-                    var aa = stat.Type switch
+                    if (stat.Player.Id == id)
                     {
-                        SummaryType.Goal => returnPlayer.Goals += 1,
-                        SummaryType.Assist => returnPlayer.Assists += 1,
-                        SummaryType.OwnGoal => returnPlayer.OwnGoals += 1,
-                        SummaryType.YellowCards => returnPlayer.YellowCards += 1,
-                        SummaryType.RedCards => returnPlayer.RedCards += 1,
-                        SummaryType.MissedPenalty => returnPlayer.MissedPenalty += 1,
-                        SummaryType.Missed10meter => returnPlayer.Missed10meter += 1,
-                        SummaryType.CleanSheetGK => returnPlayer.CleanSheetGK += 1,
-                        SummaryType.FourSavesGK => returnPlayer.FourSavesGK += 1,
-                        SummaryType.SavedFromPenaltyGK => returnPlayer.SavedFromPenaltyGK += 1,
-                        SummaryType.SavedFrom10meterGK => returnPlayer.SavedFrom10meterGK += 1
-                    };
+                        var aa = stat.Type switch
+                        {
+                            SummaryType.Goal => returnPlayer.Goals += 1,
+                            SummaryType.Assist => returnPlayer.Assists += 1,
+                            SummaryType.OwnGoal => returnPlayer.OwnGoals += 1,
+                            SummaryType.YellowCards => returnPlayer.YellowCards += 1,
+                            SummaryType.RedCards => returnPlayer.RedCards += 1,
+                            SummaryType.MissedPenalty => returnPlayer.MissedPenalty += 1,
+                            SummaryType.Missed10meter => returnPlayer.Missed10meter += 1,
+                            SummaryType.CleanSheetGK => returnPlayer.CleanSheetGK += 1,
+                            SummaryType.FourSavesGK => returnPlayer.FourSavesGK += 1,
+                            SummaryType.SavedFromPenaltyGK => returnPlayer.SavedFromPenaltyGK += 1,
+                            SummaryType.SavedFrom10meterGK => returnPlayer.SavedFrom10meterGK += 1
+                        };
 
-                    var bb = stat.Type switch
-                    {
-                        SummaryType.Goal => returnPlayer.TotalPoints += 5,
-                        SummaryType.Assist => returnPlayer.TotalPoints += 2,
-                        SummaryType.OwnGoal => returnPlayer.TotalPoints -= 3,
-                        SummaryType.YellowCards => returnPlayer.TotalPoints -= 2,
-                        SummaryType.RedCards => returnPlayer.TotalPoints -= 7,
-                        SummaryType.MissedPenalty => returnPlayer.TotalPoints -= 3,
-                        SummaryType.Missed10meter => returnPlayer.TotalPoints -= 2,
-                        SummaryType.CleanSheetGK => returnPlayer.TotalPoints += 5,
-                        SummaryType.FourSavesGK => returnPlayer.TotalPoints += 1,
-                        SummaryType.SavedFromPenaltyGK => returnPlayer.TotalPoints += 3,
-                        SummaryType.SavedFrom10meterGK => returnPlayer.TotalPoints += 2
-                    };
+                        var bb = stat.Type switch
+                        {
+                            SummaryType.Goal => returnPlayer.TotalPoints += 5,
+                            SummaryType.Assist => returnPlayer.TotalPoints += 2,
+                            SummaryType.OwnGoal => returnPlayer.TotalPoints -= 3,
+                            SummaryType.YellowCards => returnPlayer.TotalPoints -= 2,
+                            SummaryType.RedCards => returnPlayer.TotalPoints -= 7,
+                            SummaryType.MissedPenalty => returnPlayer.TotalPoints -= 3,
+                            SummaryType.Missed10meter => returnPlayer.TotalPoints -= 2,
+                            SummaryType.CleanSheetGK => returnPlayer.TotalPoints += 5,
+                            SummaryType.FourSavesGK => returnPlayer.TotalPoints += 1,
+                            SummaryType.SavedFromPenaltyGK => returnPlayer.TotalPoints += 3,
+                            SummaryType.SavedFrom10meterGK => returnPlayer.TotalPoints += 2
+                        };
+                    }
                 }
             }
-            returnPlayer.TotalPoints += returnPlayer.TotalMatches; // match attened
+            returnPlayer.TotalPoints += returnPlayer.TotalMatches * 2; // match attened
             //returnPlayer.TotalPoints += returnPlayer.Wins * 2; // team win
             returnPlayer.GoalsPerMatch = (double)returnPlayer.Goals / (double)returnPlayer.TotalMatches;
             returnPlayer.WinPerMatch = (double)returnPlayer.Wins / (double)returnPlayer.TotalMatches * 100.0;
