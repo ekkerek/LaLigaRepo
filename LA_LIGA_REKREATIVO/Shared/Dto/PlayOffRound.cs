c0da6a1a39b3d669bfb05d5da487a337f5d11355
Marko Milic <marko.milic.88@gmail.com>
2025-05-12 10:19:14 +0200

    Update enum

--- LA_LIGA_REKREATIVO/Shared/Dto/PlayOffRound.cs
+++ LA_LIGA_REKREATIVO/Shared/Dto/PlayOffRound.cs
@@ -1,11 +1,31 @@
-﻿namespace LA_LIGA_REKREATIVO.Shared.Dto
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace LA_LIGA_REKREATIVO.Shared.Dto
 {
     public enum PlayOffRound
     {
+        [Display(Name = "šesnaestina finala")]
         RoundOf32,
+        [Display(Name = "osimna finala")]
         RoundOf16,
+        [Display(Name = "četvrtfinale")]
         QuarterFinals,
+        [Display(Name = "polufinale")]
         SemiFinals,
+        [Display(Name = "Finale")]
         Final
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .FirstOrDefault()?
+                            .GetCustomAttribute<DisplayAttribute>()?
+                            .Name ?? enumValue.ToString();
+        }
     }
 }
